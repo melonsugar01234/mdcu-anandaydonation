767b0b6d3be16800e9742972c6201960522aa5a9
Akukinaqua <guynachanok2@gmail.com>
2025-03-04 16:03:25 +0700

    style:  admin login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
             <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-lg">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Enter Passphrase
+                        Admin Registeration
                     </h2>
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
