5d59a190dd92846dcdb2f287af2b81a7cf8d11f7
Akukinaqua <guynachanok2@gmail.com>
2025-03-04 15:59:01 +0700

    Admin login

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function AdminPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/admin/login");
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    return (
+      <h2>Hello Admin</h2>
+    );
+  }
+
+  return null;
+}
